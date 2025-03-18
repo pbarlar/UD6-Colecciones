37aaad3785b48a44fd112c90150f5776084bcedf
pbarlar <pbarlar@g.educaand.es>
2025-03-18 09:37:39 +0100

    update

---
+++ Ej1/Menu.java
@@ -0,0 +1,21 @@
+package Ej1;
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList<String> menu= new ArrayList<>();
+
+    public void creaOpcion(String opc){
+        menu.add(opc);
+    }
+
+    public void muestraMenu(){
+        for (String i : menu) {
+            System.out.println(i);
+        }
+    }
+
+    public void capturaOpcion(int opc){
+        System.out.println(menu.get(opc));
+    }
+   
+}
---
+++ Ej1/TestMenu.java
@@ -0,0 +1,12 @@
+package Ej1;
+public class TestMenu {
+    public static void main(String[] args) {
+        Menu menu1=new Menu();
+
+        menu1.creaOpcion("1 - Elegir acompañante");
+        menu1.creaOpcion("2 - Eliminar acompañante");
+        
+        menu1.muestraMenu();
+        menu1.capturaOpcion(1);
+    }
+}
